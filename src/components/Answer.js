50d0e3e763afc2741df06203da9b9fecbe8fcb32
James Robbs <jamesrobbs01@gmail.com>
2022-08-01 00:07:16 +0100

    minor styling tweaks

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -4,7 +4,7 @@ import './Answer.css'
 export default function Answer(props) {
 
   const styles = {
-    border: props.correct && props.submitted && !props.selected ? "2px solid green" : props.selected? "none" : "0.9px solid #4D5B9E",
+    border: props.correct && props.submitted && !props.selected ? "2px solid green" : props.selected? "none" : "1px solid #4D5B9E",
     backgroundColor: props.selected && props.submitted && !props.correct ? "pink" : props.selected ? "rgb(155, 209, 155)" : "transparent"
   }
 
